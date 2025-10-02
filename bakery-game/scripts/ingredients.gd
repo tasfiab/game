334806f14ec50f9c771909fd458720d18c66e924
tasfiab <tbhuiyan@students.mags.school.nz>
2025-10-03 11:48:28 +1300

    ingredients tweening, visuals improved

--- bakery-game/scripts/ingredients.gd
+++ bakery-game/scripts/ingredients.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+@export var strawberry : TextureRect
+@export var lemon : TextureRect
+@export var chocolate : TextureRect
+@export var vanilla : TextureRect
+@export var cake : TextureRect
+@export var bread : TextureRect
+
+
 var can_click_cake : bool = false
 var can_click_bread : bool = false
 var can_click_strawberry : bool = false
@@ -33,18 +41,18 @@ func _ready() -> void:
 	for ingredient in Global.chosen_ingredients:
 		Global.ingredient_chosen = false
 		if ingredient_number == 0:
-			$strawberry.hide()
-			$lemon.hide()
-			$chocolate.hide()
-			$vanilla.hide()
+			strawberry.hide()
+			lemon.hide()
+			chocolate.hide()
+			vanilla.hide()
 		elif ingredient_number == 1:
-			$vanilla.show()
-			$chocolate.show()
+			vanilla.show()
+			chocolate.show()
 		elif ingredient_number == 2:
-			$strawberry.show()
-			$lemon.show()
-			$vanilla.hide()
-			$chocolate.hide()
+			strawberry.show()
+			lemon.show()
+			vanilla.hide()
+			chocolate.hide()
 		
 		await ingredient_clicked
 		ingredient_number += 1
@@ -52,14 +60,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Global.chosen_ingredients[0] == BREAD:
-		$cake_essence.hide()
+		cake.hide()
 	if Global.chosen_ingredients[0] == CAKE:
-		$bread_essence.hide()
+		bread.hide()
 	
 	if Global.chosen_ingredients[0] == "":
 		$Label.text = ""
-		$cake_essence.show()
-		$bread_essence.show()
+		cake.show()
+		bread.show()
 		
 	if Input.is_action_just_pressed("interact") and Global.chosen_ingredients.has(""):
 		if can_click_cake:
@@ -103,55 +111,73 @@ func _process(delta: float) -> void:
 func _choosing_ingredients(ingredient : String, index : int):
 	Global.chosen_ingredients[index] = ingredient
 	
+func _hover_tween(ingredient):
+	var tween = create_tween()
+	tween.tween_property(ingredient, "scale", Vector2(1.1,1.1),0.1)
+	pass
+
+func _not_hover_tween(ingredient):
+	var tween = create_tween()
+	tween.tween_property(ingredient, "scale", Vector2(1,1),0.1)
+	pass
+	
 
 
 
 func _on_cake_essence_mouse_entered() -> void:
 	can_click_cake = true
+	_hover_tween(cake)
 
 
 func _on_cake_essence_mouse_exited() -> void:
 	can_click_cake = false
-
+	_not_hover_tween(cake)
 
 func _on_bread_essence_mouse_entered() -> void:
 	can_click_bread = true
-
+	_hover_tween(bread)
 
 func _on_bread_essence_mouse_exited() -> void:
 	can_click_bread = false
+	_not_hover_tween(bread)
 
 
 func _on_strawberry_mouse_entered() -> void:
 	can_click_strawberry = true
+	_hover_tween(strawberry)
 
 
 func _on_strawberry_mouse_exited() -> void:
 	can_click_strawberry = false
+	_not_hover_tween(strawberry)
 
 
 func _on_lemon_mouse_entered() -> void:
 	can_click_lemon = true
+	_hover_tween(lemon)
 
 
 func _on_lemon_mouse_exited() -> void:
 	can_click_lemon = false
-
+	_not_hover_tween(lemon)
 
 func _on_vanilla_mouse_entered() -> void:
 	can_click_vanilla = true
+	_hover_tween(vanilla)
 
 
 func _on_vanilla_mouse_exited() -> void:
 	can_click_vanilla = false 
-
+	_not_hover_tween(vanilla)
 
 func _on_chocolate_mouse_entered() -> void:
 	can_click_chocolate = true
+	_hover_tween(chocolate)
 	
 
 func _on_chocolate_mouse_exited() -> void:
 	can_click_chocolate = false
+	_not_hover_tween(chocolate)
 
 
 func _on_done_button_pressed() -> void:
